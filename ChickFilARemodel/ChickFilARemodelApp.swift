2aaa44e7a835f7a8dfeade894666373beca93bfb
Bryce King <bryceeking5@gmail.com>
2024-10-27 20:05:56 -0400

    Initial Commit

---
+++ ChickFilARemodel/ChickFilARemodelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChickFilARemodelApp.swift
+//  ChickFilARemodel
+//
+//  Created by Bryce King on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ChickFilARemodelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
